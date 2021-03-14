0e5f31415a097f089e583cdbfde4fc26ffae0ac5
Kirill <perevyshink_1@mail.ru>
2021-03-14 18:55:51 +0600

    added aditional styles

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,6 +5,10 @@ const calculatorUrl = "http://localhost:8080/calculate"
 async function getFinalFunc() {
     const averageMark = document.getElementById('averageMark').value
     const examMark = document.getElementById('examMark').value
+    if (averageMark.trim() === "" || examMark.trim() === "") {
+        alert("Не все данные были введены")
+        return
+    }
     const data = {
         averageMark,
         examMark
@@ -16,5 +20,15 @@ async function getFinalFunc() {
         		'Content-Type': 'application/json'
 			}, 
 			body: JSON.stringify(data)
-        }).then(response => response.json()).then(data => responseBox.innerText = data);
+        }).then(response => response.json()).then(data => {
+            const finalMark = parseFloat(data)
+                if (finalMark >= 70.0 && finalMark <= 100.0) {
+                    responseBox.classList.add("bg-success")
+                } else if (finalMark >= 50.0 && finalMark <= 69.0) {
+                    responseBox.classList.add("bg-warning")
+                }else {
+                    responseBox.classList.add("bg-danger")
+                }
+            responseBox.innerText = data
+        });
 }
